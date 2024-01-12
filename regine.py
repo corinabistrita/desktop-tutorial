39cf04602e665267cc87bbbacdbacfc1746c7ef4
corinabistrita <88577871+corinabistrita@users.noreply.github.com>
2024-01-12 12:48:26 +0200

    Add afisarea aparte in functie Improve fc vizualizare

--- regine.py
+++ regine.py
@@ -1,4 +1,5 @@
 import time
+from colorama import Fore, Style
 
 numar_regine = 8
 solutie_curenta = [0 for x in range(numar_regine)]
@@ -24,13 +25,32 @@ def pozitionare_solutii(row):
             solutie_curenta[row] = col
             if row == (numar_regine - 1):
                 solutii.append(solutie_curenta.copy())
-                print("Solution number", len(solutii), solutie_curenta)
             else:
                 pozitionare_solutii(row + 1)
 
+def afisare(solution):
+    print("+---" * numar_regine + "+")
+    for row in range(numar_regine):
+        line = "|"
+        for col in range(numar_regine):
+            if col == solution[row]:
+                line += f" {Fore.RED}♛{Style.RESET_ALL} |"
+            else:
+                line += " . |"
+        print(line)
+        print("+---" * numar_regine + "+")
 
+def vizualizare():
+    global solutii
+    for idx, solutie in enumerate(solutii):
+        print(f"{Fore.GREEN}Solutia numarul: {idx + 1}{Style.RESET_ALL}")
+        afisare(solutie)
+        print("\n")
+
+print("Gasirea solutiilor " + str(numar_regine) + " Regine ")
 time.sleep(2)
 pozitionare_solutii(0)
+print(len(solutii), "solutions found")
 
-for solutie in solutii:
-    print(solutie)
+
+vizualizare()
