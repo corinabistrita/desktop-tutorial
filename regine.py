6f1a99e545e00707c585d22d8b8f7e89fea31a86
corinabistrita <88577871+corinabistrita@users.noreply.github.com>
2024-01-12 05:19:26 +0200

    Rezolvarea erorii de logica /Gasirea tuturor solutiilor

--- regine.py
+++ regine.py
@@ -1,54 +1,40 @@
 numar_Regine = 8
 solutii = []
 
+def afisare_solutie(board):
+    for row in board:
+        print(row)
+    print()
+
 def pozitii_corecte(board, row, col):
-    for i in range(col):
-        if board[row][i] == 1:
+    for i in range(row):
+        if board[i][col] == 1 or \
+           (col - (row - i) >= 0 and board[i][col - (row - i)] == 1) or \
+           (col + (row - i) < numar_Regine and board[i][col + (row - i)] == 1):
             return False
-
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    
-    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False #diag,inferioara
-
     return True
 
-def rezolvare()
+def rezolvare(board, row):
+    global numar_Regine, solutii
+
+    if row == numar_Regine:
+        solutii.append([r[:] for r in board])
+        return
+
+    for col in range(numar_Regine):
+        if pozitii_corecte(board, row, col):
+            board[row][col] = 1
+            rezolvare(board, row + 1)
+            board[row][col] = 0
+
+def gaseste_solutii():
+    global numar_Regine, solutii
 
     board = [[0 for _ in range(numar_Regine)] for _ in range(numar_Regine)]
-    stiva = [(0, 0)] 
+    rezolvare(board, 0)
 
-    while stiva:
-        row, col = stiva.pop()
-
-        while col < numar_Regine:
-            if pozitii_corecte(board, row , col):
-                board[row][col] = 1
-                stiva.append((row, col + 1))  
-                break
-            else:
-                col += 1
-
-        if col == numar_Regine:
-            
-            if stiva:
-                row, _ = stiva.pop()
-                col += 1
-                board[row][col - 1] = 0  
-            else:
-                
-                break
-
-        if row == numar_Regine - 1 and col == numar_Regine:
-           
-            solutii.append([row[:] for row in board])
-
-rezolvare()
+gaseste_solutii()
 
 print(len(solutii), "solutii gasite")
 for solutie in solutii:
-    print(solutie)
+    afisare_solutie(solutie)
