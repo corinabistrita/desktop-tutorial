1a875d596e54cb6bb5f83d72c7535873da64f9f2
corinabistrita <88577871+corinabistrita@users.noreply.github.com>
2024-01-13 16:46:26 +0200

    Perfectare meniului interaciv

--- regine.py
+++ regine.py
@@ -18,13 +18,13 @@ def pozitionare_solutii(row, numar_regine, solutie_curenta, solutii):
         else:
             solutie_curenta[row] = col
             if row == (numar_regine - 1):
-                solutii.append(solutie_curenta.copy())
+                solutii.append([x for x in solutie_curenta])
             else:
                 pozitionare_solutii(row + 1, numar_regine, solutie_curenta, solutii)
 
 def gaseste_solutii(numar_regine):
     solutii = []
-    solutie_curenta = [0 for x in range(numar_regine)]
+    solutie_curenta = [0] * numar_regine
     pozitionare_solutii(0, numar_regine, solutie_curenta, solutii)
     return solutii
 
@@ -50,29 +50,34 @@ def salvare_in_fisier(solutii, nume_fisier="solutii_regine.txt"):
     with open(nume_fisier, "w", encoding="utf-8") as file:
         for idx, solutie in enumerate(solutii):
             file.write(f"Solutia numarul: {idx + 1}\n")
-            for row in solutie:
-                line = "|"
-                for col in range(len(solutie)):
-                    if col == row:
-                        line += f" ♛ |"
-                    else:
-                        line += " . |"
-                file.write(line + "\n")
-                file.write("+---" * len(solutie) + "+\n")
+            file.write(tabla_to_str(solutie))
             file.write("\n")
 
+def tabla_to_str(tabla):
+    result = ""
+    for row in tabla:
+        line = "|"
+        for col in range(len(tabla)):
+            if col == row:
+                line += f" ♛ |"
+            else:
+                line += " . |"
+        result += line + "\n" + "+---" * len(tabla) + "+\n"
+    return result
 
-def main():
+def get_valid_numar_regine():
     while True:
         try:
             numar_regine = int(input("Introduceți numărul de regine (între 4 și 10): "))
             if 4 <= numar_regine <= 10:
-                break
+                return numar_regine
             else:
                 print("Numărul de regine trebuie să fie între 4 și 10. Reîncercați.")
         except ValueError:
             print("Introduceți un număr valid.")
 
+def main():
+    numar_regine = get_valid_numar_regine()
     print(f"Gasirea solutiilor pentru {numar_regine} Regine ")
     time.sleep(2)
     solutii = gaseste_solutii(numar_regine)
